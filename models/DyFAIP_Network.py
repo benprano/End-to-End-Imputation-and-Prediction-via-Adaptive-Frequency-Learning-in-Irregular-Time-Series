4f1d1b2e1d7c2fbcad32261c546b5ffc31707590
benprano <45176454+benprano@users.noreply.github.com>
2025-06-05 23:00:42 +0200

    Update DyFAIP_Network.py

--- models/DyFAIP_Network.py
+++ models/DyFAIP_Network.py
@@ -151,7 +151,7 @@ class DyFAIP_Cell(torch.jit.ScriptModule):
         # Compute imputed values %factor_imp >= threshold, or any value from grid search
         # any value from 0.90 to 0.99 give the similar results as more than 90% features are selected based on this threshold settings.
         # (0.99 ---> select almost all the features to be used)
-        omega = 0.97  
+        omega = 0.99  
         threshold = omega * factor_imp.max()
 
         # Compute imputed values
